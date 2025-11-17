fc8ab1e0402908960f0feb9c26bf4aa059c3b7cd
Naiomi Naidoo <77915440+C0d3N1nJ4@users.noreply.github.com>
2025-11-17 12:07:50 +0000

    refactor: use Lombok annotations in PaymentBuilderProperties
    - Add @Getter and @RequiredArgsConstructor to main class
    - Add @Data to Input and Output inner classes
    - Remove manual getter/setter methods
    - Change from field initialization to constructor injection

--- src/main/java/com/naiomi/payment/builder/config/PaymentBuilderProperties.java
+++ src/main/java/com/naiomi/payment/builder/config/PaymentBuilderProperties.java
@@ -3,42 +3,26 @@ package com.naiomi.payment.builder.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @ConfigurationProperties(prefix = "payment.builder")
+@Getter
+@RequiredArgsConstructor
 public class PaymentBuilderProperties {
 
-    private final Input input = new Input();
-    private final Output output = new Output();
+    private final Input input;
+    private final Output output;
 
-    public Input getInput() {
-        return input;
-    }
-
-    public Output getOutput() {
-        return output;
-    }
-
+    @Data
     public static class Input {
         private String directory;
-
-        public String getDirectory() {
-            return directory;
-        }
-
-        public void setDirectory(String directory) {
-            this.directory = directory;
-        }
     }
 
+    @Data
     public static class Output {
         private String directory;
-
-        public String getDirectory() {
-            return directory;
-        }
-
-        public void setDirectory(String directory) {
-            this.directory = directory;
-        }
     }
 }
